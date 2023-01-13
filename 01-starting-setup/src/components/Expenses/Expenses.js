0fbc4717bcdbfd7fd338a7aea5f8e9225310509e
Brian Ackermann <brian.ackermann@gmail.com>
2023-01-12 21:40:40 -0600

    finished 59 and assignment 2 - lifting the state up

--- 01-starting-setup/src/components/Expenses/Expenses.js
+++ 01-starting-setup/src/components/Expenses/Expenses.js
@@ -1,33 +1,46 @@
+import React , {useState} from "react";
 import "./Expenses.css";
 import ExpenseItem from "./ExpenseItem";
 import Card from "../UI/Card";
+import ExpensesFilter from "./ExpensesFilter";
 
 function Expenses(props) {
-  return (
-    <Card className="expenses">
-      <h2>expenses</h2>
 
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+  const [yearFilter, setYearFilter] = useState('2022');
+
+  const FilterChangeHandler = (filterYear) => {
+    console.log("recieved FilterChange update", filterYear);
+    setYearFilter(filterYear);
+  };
+
+  return (
+    <div>
+      <Card className="expenses">
+        
+        <ExpensesFilter selectedYear={yearFilter} onFilterChange={FilterChangeHandler}/>
+
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </div>
   );
 }
 
